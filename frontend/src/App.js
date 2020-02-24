de8c7065623ffeeb0800d88bcd8798a44e0b34e6
DONADEI Emrick <emrick.donadei@hotmail.fr>
2020-02-24 23:44:35 +0100

    starting page for repository handler, need to emulate console and to query backend !

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { makeStyles } from "@material-ui/core/styles";
 import { getRepositoriesFromUser } from "./services/GithubAPI";
-import { Button, Grid, TextField, Typography } from "@material-ui/core";
+import { Button, Grid, TextField, Typography, Paper } from "@material-ui/core";
 import { ToolbarApp } from "./components/ToolbarApp";
 import { RepoList } from "./components/RepoList";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ClipLoader from "react-spinners/ClipLoader";
 
 const useStyles = makeStyles({
   table: {
     minWidth: 650
   },
-  root: {
+  button: {
     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
     borderRadius: 3,
     border: 0,
@@ -23,31 +23,44 @@ const useStyles = makeStyles({
   },
   container: {
     paddingTop: 40
+  },
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    height: 140,
+    width: 100
+  },
+  control: {
+    padding: 2
   }
 });
 
 function App() {
   const classes = useStyles();
-  const [connected, setConnected] = useState(false);
+  const [repository, setRepository] = useState({});
   const [nameOfUser, setnameOfUser] = useState("edonadei");
   const [repositories, setRepositories] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    getRepositoriesFromUser(nameOfUser).then(data => {
-      setLoading(false);
-      setRepositories(data);
-    });
-  }, [nameOfUser]);
+  useEffect(
+    () => {
+      setLoading(true);
+      getRepositoriesFromUser(nameOfUser).then(data => {
+        setLoading(false);
+        setRepositories(data);
+      });
+    },
+    [nameOfUser]
+  );
 
   return (
     <React.Fragment>
       <Router>
         <ToolbarApp />
-        <Switch>
-          <Route exact path="/">
-            <div className={classes.container}>
+        <div className={classes.container}>
+          <Switch>
+            <Route exact path="/">
               <Grid
                 container
                 direction="column"
@@ -60,20 +73,74 @@ function App() {
                   onChange={e => {
                     setnameOfUser(e.target.value);
                   }}
-                  style={{paddingBottom: 30}}
-                ></TextField>
-                {loading ? (
-                  <ClipLoader size={150} color={"#123abc"} loading={loading} />
-                ) : (
-                  <RepoList repositories={repositories} />
-                )}
+                  style={{ paddingBottom: 30 }}
+                />
+                {loading
+                  ? <ClipLoader
+                      size={150}
+                      color={"#123abc"}
+                      loading={loading}
+                    />
+                  : <RepoList
+                      repositories={repositories}
+                      setRepository={setRepository}
+                    />}
               </Grid>
-            </div>
-          </Route>
-          <Route path="/pytoolchain">
-            <Typography>Hello from /pytoolchain</Typography>
-          </Route>
-        </Switch>
+            </Route>
+            <Route path="/toolchain">
+              <Grid container className={classes.root} spacing={2}>
+                <Grid item xs={12}>
+                  <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    spacing={5}
+                  >
+                    <Grid item>
+                      <Typography variant="h5">
+                        Project name: {repository?.name}
+                      </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography variant="h5">
+                        Github: {repository?.url}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                  <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    spacing={2}
+                  >
+                    <Grid item>
+                      <Paper>
+                        <Button className={classes.button}>Build</Button>
+                        <Typography>Console</Typography>
+                      </Paper>
+                    </Grid>
+                    <Grid item>
+                      <Paper>
+                        <Button className={classes.button}>Test</Button>
+                        <Typography>Console</Typography>
+                      </Paper>
+                    </Grid>
+                    <Grid item>
+                      <Paper>
+                        <Button className={classes.button}>Deploy</Button>
+                        <Typography>Console</Typography>
+                      </Paper>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Route>
+          </Switch>
+        </div>
       </Router>
     </React.Fragment>
   );
