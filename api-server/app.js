415f11ca6124f71b8477f233e05a0fa9dde21a77
DONADEI Emrick <emrick.donadei@hotmail.fr>
2020-02-27 10:37:45 +0100

    init nodemon, babel and mongoose schemas, and first post query

--- api-server/app.js
+++ api-server/app.js
@@ -1,22 +1,67 @@
-const express = require("express");
-const app = express();
+import mongoose from "mongoose";
+import body from "body-parser";
+const app = require("express")();
+app.use(body());
 
-app.get("/", function(req, res) {
+mongoose.connect("mongodb://localhost:27017/builds", { useNewUrlParser: true });
+
+// Schema build
+const schemaBuild = mongoose.Schema({
+  repoUrl: String,
+  id: String,
+  env_vars: {
+    type: Map,
+    of: String
+  }
+});
+
+// Schema test
+const schemaTest = mongoose.Schema({
+  repoUrl: String,
+  id: String,
+  choosenTests: [String]
+});
+
+// Schema deploy
+const schemaDeploy = mongoose.Schema({
+  repoUrl: String,
+  id: String,
+  cloudProviders: [String]
+});
+
+app.get("/", (req, res) => {
   res.send("The API Server is reachable !");
 });
 
-app.get("/build", function(req, res) {
+app.get("/build", (req, res) => {
   res.send("Building code !");
 });
 
-app.get("/test", function(req, res) {
+app.post("/build", async (req, res) => {
+  build = {
+    repo_url: req.body.repo_url,
+    action: req.body.action,
+    id: req.body.action
+  };
+
+  if (!build.repo_url || !build.action || !build.id) {
+    res.send("Missing argument");
+    return;
+  }
+
+  await build.save() // saving into mongo
+    res.json(build)
+    return
+});
+
+app.get("/test", (req, res) => {
   res.send("Testing code !");
 });
 
-app.get("/deploy", function(req, res) {
+app.get("/deploy", (req, res) => {
   res.send("Deploying code !");
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log("api server for k8s controller running on port 3000!");
 });
